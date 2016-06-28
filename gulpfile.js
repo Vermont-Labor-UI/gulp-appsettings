0b141a5fd3b09e0a0205c1853ca8a1ec3850e8b8
Rocotton <Robert.Cotton@labor.idaho.gov>
2016-06-28 10:10:02 -0600

    Initial appsettings package

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+/// <binding />
+"use strict";
+var gulp = require("gulp");
+var debug = require('gulp-debug');
+
+var BOWER_COMPONENTS = global.BowerComponents || "wwwroot/lib";
+
+gulp.task('appsettings', function() {
+        return gulp.src(['./' + BOWER_COMPONENTS + '/iUS.AppSettings/*.json'])
+            .pipe(debug())
+            .pipe(gulp.dest('./Configuration/'));
+});
